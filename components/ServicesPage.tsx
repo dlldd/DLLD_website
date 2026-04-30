17db756c1f41ae51979e74c18eff7605fa76d2ca
dlldd <dllddesignstudio@gmail.com>
2026-04-30 20:21:09 +0900

    feat: Update services data and app version
    Updates the `CURRENT_VERSION` in `App.tsx` to '7.5' to trigger local storage reset.
    Modifies the `ServicesPage.tsx` component to update image URL and `projectIds` for the 'Branding' service category, reflecting recent project additions or changes.

--- components/ServicesPage.tsx
+++ components/ServicesPage.tsx
@@ -89,9 +89,9 @@ const servicesData: Record<ServiceCategory, ServiceContent> = {
     ],
     images: [
       'https://raw.githubusercontent.com/dlldd/DLLD-web/main/%E1%84%82%E1%85%A9%E1%84%90%E1%85%B3%E1%84%91%E1%85%A9%E1%86%AF%E1%84%85%E1%85%B5%E1%84%8B%E1%85%A9_gs-02.jpg',
-      'https://raw.githubusercontent.com/dlldd/DLLD-web/main/%E1%84%82%E1%85%A9%E1%84%90%E1%85%B3%E1%84%91%E1%85%A9%E1%86%AF%E1%84%85%E1%85%B5%E1%84%8B%E1%85%A9_%E1%84%8E%E1%85%B5%E1%84%8E%E1%85%A1%E1%84%8E%E1%85%A9%E1%84%91%E1%85%A2%E1%84%8F%E1%85%B5%E1%84%8C%E1%85%B5-01.jpg'
+      'https://raw.githubusercontent.com/dlldd/DLLD-web/main/yp_5.jpg'
     ],
-    projectIds: ['5', '8']
+    projectIds: ['5', '14']
   }
 };
 
